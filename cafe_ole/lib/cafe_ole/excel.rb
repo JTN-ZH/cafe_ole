741550200d4fa5bc10d54544d061449e6002259c
JTN-ZH <37428143+JTN-ZH@users.noreply.github.com>
2018-04-04 16:55:52 +0900

    excel module
    commit

---
+++ cafe_ole/lib/cafe_ole/excel.rb
@@ -0,0 +1,21 @@
+# coding: UTF-8
+
+module Excel
+  def test(arg1, *args, a:4, **hash, &blk)
+    p arg1
+    p args
+    p a
+    p hash
+    p "block" if blk
+    yield blk if blk
+  end
+ 
+  test(1, 2, 3, b:5, c:6) { p :test }
+end
+
+
+##########################################
+if __FILE__ == $0
+  
+end
+##########################################
